d7374f5a090c177686dde55487ff3b001e9dbdfd
Matthew <space-explorer@hotmail.com>
2015-10-14 15:15:25 -0500

    trying again

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
-tar xf cmake-3.2.2.tar.gz
+tar -xzvf cmake-3.2.2.tar.gz
 cd cmake-3.2.2
-./configure
+./configure --prefix=$HOME/cmake-3.2.2
 make
 make install
 
