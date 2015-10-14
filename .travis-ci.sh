5c6572a4ba558cc95551759bb5ccb5f5e2081ff2
Matthew <space-explorer@hotmail.com>
2015-10-14 13:16:52 -0500

    travis intregration, try #1

---
+++ .travis-ci.sh
@@ -0,0 +1,8 @@
+PACKAGE=jet
+
+echo "yes" | sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main'
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+
+sudo apt-get update -qq
+sudo apt-get install -qq -y llvm-3.7 clang-3.7
+
